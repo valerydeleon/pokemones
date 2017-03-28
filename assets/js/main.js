75ff0f5d96c6bfeb3e4ec3adb055db5539f58dee
Valeria de Leon <valery.deleon@gmail.com>
2017-03-28 17:50:34 -0600

    anade elemento a arreglo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-alert("si funciona")
+
+
+var arregloPokemon = [];
+
+function Pokemon(nombre, color, nivelAtaque){//objeto de pokemon
+  this.nombre = nombre;
+  this.color = color;
+  this.nivelAtaque = nivelAtaque;
+  this.vida = 100;
+
+  this.peleaPokemon = function(objetoDePokemon){
+    objetoDePokemon.vida = objetoDePokemon.vida - this.nivelAtaque;
+  }
+};
+
+function crearPokemon(){
+  //agrega a arreglo un nuevo pokemon
+  var nombrePokemon = document.getElementById("nombrePokemon");
+  var colorPokemon = document.getElementById("colorPokemon");
+  var nivelAtaquePokemon = document.getElementById("nivelAtaquePokemon");
+
+  var pokemon = new Pokemon(
+    nombrePokemon.value,
+    colorPokemon.value,
+    parseInt(nivelAtaquePokemon.value)
+  )
+
+  arregloPokemon.push(pokemon);
+};
+
+console.log()
